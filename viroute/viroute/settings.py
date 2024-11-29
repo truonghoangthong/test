b1a422795634498e7bfdba61b6ab24dd2bcb447c
truonghoangthong <126312478+truonghoangthong@users.noreply.github.com>
2024-11-30 00:35:05 +0200

    f

--- viroute/viroute/settings.py
+++ viroute/viroute/settings.py
@@ -189,5 +189,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'banhmingon90@gmail.com'
 EMAIL_HOST_PASSWORD = 'chithich1nguoi'
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173',
+]
+
 
